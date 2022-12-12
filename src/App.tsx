569b412c517e7703a0150afeb70976c5978550c1
levylima <levylima1510@gmail.com>
2022-12-11 21:01:05 -0300

    creating post, put, delete in the post and theme

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import { Login } from './paginas/login/Login';
 import Home from './paginas/home/Home';
 import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
   return (
@@ -26,6 +30,18 @@ function App() {
         <Route path='/temas' element={< ListaTema />} />
 
         <Route path='/posts' element={<ListaPostagem />} />
+
+        <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+        <Route path="/formularioTema" element={<CadastroTema />} />
+
+        <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+        <Route path="/deletarTema/:id" element={<DeletarTema />} />
 
       </Routes>
       < Footer />
